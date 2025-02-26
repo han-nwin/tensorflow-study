c6f1c4fbe7d16468138e8b25ecdb630fefb4e165
han-nwin <hannguyen.win@gmail.com>
2025-02-25 23:51:16 -0600

    2x-1

--- examples/2x-1.py
+++ examples/2x-1.py
@@ -15,7 +15,7 @@ xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
 # Train the model
-model.fit(xs, ys, epochs=500)
+model.fit(xs, ys, epochs=500)  # epochs = how many time of training
 
 # Make a prediction
 print("500 training Prediction of y=2x-1. Test x = 10")
